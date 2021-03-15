932b0b162d4e82e756b911635de5f9c3c903db9e
ReesLuke <69607075+ReesLuke@users.noreply.github.com>
2021-03-15 16:50:42 -0400

    Update data.tsx

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,27 @@
+export default {
+    products: [{
+        name: 'Shirt',
+        imageURL: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageURL: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Belt',
+        imageURL: '/images/d1.jpg',
+        price: 30,
+        brand: 'Adidas',
+        rating: 4.6,
+        numberOfReviews: 10
+    }]
+    
+}
