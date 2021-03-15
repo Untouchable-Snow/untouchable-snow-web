a2254c73ed1177a85fce60ccdbc669223ad85d5d
ReesLuke <69607075+ReesLuke@users.noreply.github.com>
2021-03-15 16:51:26 -0400

    Update Products.tsx

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import data from './data';
+
 function Products() {
     return (
-        <div>
-            <h2>Products</h2>
+        <div className="content">
+            <ul className="products">
+                {
+                    data.products.map(product =>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src={product.imageURL} alt="product" />
+                                <div className="product-name">
+                                    <a href="product.html">{product.name}</a>
+                                </div>
+                                <div className="product-brand">{product.brand}</div>
+                                <div className="product-price">{product.price}</div>
+                                <div className="product-rating">{product.rating} Stars ({product.numberOfReviews})</div>
+                            </div>
+                        </li>)
+                }
+            </ul>
         </div>
     );
 }
